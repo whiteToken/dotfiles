be8ea028fc371f6de2ab2ffe096c78b830acffb3
chris <chris@crunchbang>
2013-11-29 02:01:23 -0500

    adding redundent fontcofig dir

--- insdot.sh
+++ insdot.sh
@@ -5,5 +5,8 @@ cp -v .vimrc ~/
 cp -rv .vim ~/
 cp -rv .fonts ~/
 
+#one of these should be the font config dir
 mkdir -pv ~/.config/fontconfig/conf.d
+mkdir -pv ~/.fonts.conf.d
 cp -v 10-powerline-symbols.conf ~/.config/fontconfig/conf.d
+cp -v 10-powerline-symbols.conf ~/.fonts.conf.d
