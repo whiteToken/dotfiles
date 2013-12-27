e4e129e312dd3467d72644772e757fae290d3589
chris <chris.vonzuben@gmail.com>
2013-12-27 16:36:17 -0500

    update insdot.sh automaticly get plugins from git

--- insdot.sh
+++ insdot.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 # copy files for per user vim config
 
-cp -v .vimrc ~/
-cp -rv .vim ~/
-cp -rv .fonts ~/
+if [ ! $# -eq 0 ]; then
+    cp -v .vimrc ~/
+    cp -rv .vim ~/
+    cp -rv .fonts ~/
+    mkdir -pv ~/.config/fontconfig/conf.d
+    cp -v 10-powerline-symbols.conf ~/.config/fontconfig/conf.d
 
-#one of these should be the font config dir
-mkdir -pv ~/.config/fontconfig/conf.d
-mkdir -pv ~/.fonts.conf.d
-cp -v 10-powerline-symbols.conf ~/.config/fontconfig/conf.d
-cp -v 10-powerline-symbols.conf ~/.fonts.conf.d
+
+    if [ $1 == "-i" ]; then
+        git clone https://github.com/bling/vim-airline ~/.vim/bundle/vim-airline
+        git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+    fi
+    if [ $1 == "-g" ]; then
+        git pull https://github.com/bling/vim-airline ~/.vim/bundle/vim-airline
+        git pull https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+    fi
+
+    echo "run fc-cache -fv to update fonts";
+else
+    echo "-i for initial install"
+    echo "-g for update with git"
+    echo "-u for update with out git" #anything but -i or -g will do
+fi
